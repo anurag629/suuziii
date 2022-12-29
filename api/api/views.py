446aff05423ca9195daf69c42c4f55237a1d39ec
anurag629 <anurag629@gmail.com>
2022-12-29 22:27:13 +0530

    created basic app

--- api/api/views.py
+++ api/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Person
+from .serializers import PersonSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class PersonList(generics.ListCreateAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
